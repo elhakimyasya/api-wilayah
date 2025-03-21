e3a963da733afa29b0990e13dd05ff8e1c0ae82b
Yasya El Hakim <yasyaelhakim@gmail.com>
2025-03-21 12:57:51 +0700

    Added Province Logo - update 3 files and create 38 files

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { NextRequest, NextResponse } from 'next/server';
 import { mappingWilayah } from '@/utils/reader';
 
@@ -7,15 +9,25 @@ export async function GET(req: NextRequest) {
         const search = req.nextUrl.searchParams.get('q');
 
         if (!fullMapping.provinsi || Object.keys(fullMapping.provinsi).length === 0) {
-            return NextResponse.json({ message: 'Data provinsi tidak ditemukan!' }, { status: 404 });
+            return NextResponse.json({
+                message: 'Data provinsi tidak ditemukan!'
+            }, {
+                status: 404
+            });
         }
 
         let result = Object.entries(fullMapping.provinsi).map(([id, nama]) => {
             const idProvinsi = String(id).padStart(2, '0');
             const jumlah_kabupaten = fullMapping.kabupaten?.[Number(id)] ? Object.keys(fullMapping.kabupaten[Number(id)]).length : 0;
 
+            const logoPath = path.join(process.cwd(), 'public', 'images', 'provinsi', `${idProvinsi}.png`);
+            const logoUrl = fs.existsSync(logoPath) ? `${req.nextUrl.protocol}//${req.nextUrl.host}/images/provinsi/${idProvinsi}.png` : null;
+
             return {
-                id_provinsi: idProvinsi, nama, jumlah_kabupaten,
+                id_provinsi: idProvinsi,
+                nama,
+                logo: logoUrl,
+                jumlah_kabupaten,
             };
         });
 
@@ -27,10 +39,16 @@ export async function GET(req: NextRequest) {
             return NextResponse.json({ message: 'Hasil Tidak Ditemukan.' }, { status: 200 });
         }
 
-        return NextResponse.json(result, { status: 200 });
+        return NextResponse.json(result, {
+            status: 200
+        });
 
     } catch (error) {
         console.error('Error saat memproses permintaan:', error);
-        return NextResponse.json({ message: 'Terjadi kesalahan pada server' }, { status: 500 });
+        return NextResponse.json({
+            message: 'Terjadi kesalahan pada server'
+        }, {
+            status: 500
+        });
     }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+const baseUrl = typeof window !== "undefined" ? window.location.origin : "";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,8 +18,8 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto" href="https://elc-api-wilayah.vercel.app/api" target="_blank" rel="noopener noreferrer">
-            Lihat API
+          <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto" href={`${baseUrl}/api`} target="_blank" rel="noopener noreferrer">
+            Buka API
           </a>
 
           <a className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto" href="https://github.com/elhakimyasya/api-wilayah" target="_blank" >
